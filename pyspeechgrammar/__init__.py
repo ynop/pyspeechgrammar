f1292b77f65b1a7b3f61f57f5936540f3a174d2f
buec <buec@zhaw.ch>
2015-10-01 13:39:20 +0200

    added function to convert jsgf file to srgs xml file

--- pyspeechgrammar/__init__.py
+++ pyspeechgrammar/__init__.py
@@ -4,7 +4,6 @@ __version__ = '0.1.0'
 __author__ = 'Buechi Matthias <m.buechi@outlook.com>'
 __all__ = []
 
-
 from pyspeechgrammar import jsgf
 from pyspeechgrammar import srgs_xml
 
@@ -15,3 +14,11 @@ def convert_jsgf_string_to_srgs_xml_string(jsgf_string):
 
     s = srgs_xml.SRGSXMLParser()
     return s.write_string(m)
+
+
+def convert_jsgf_to_srgs_xml(jsgf_input_file, srgs_output_file):
+    p = jsgf.JSGFParser()
+    m = p.parse_from_file(jsgf_input_file)
+
+    s = srgs_xml.SRGSXMLParser()
+    return s.write_to_file(m, srgs_output_file)
